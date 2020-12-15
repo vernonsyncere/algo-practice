ab0ed244ca140588556e4fada20d02c263c3861b
Vernon Lewis <leesyncere@gmail.com>
2020-12-15 03:09:04 -0800

    subset or an array

--- December/Redo.py
+++ December/Redo.py
@@ -5,19 +5,39 @@
 # if the the sum is less than the target we move left pointers
 # if sum is greater than target we move the right
 
-def twoSum(array, target):
-   array.sort()
-   lIdx = 0
-   rIdx = len(array) - 1
+# def twoSum(array, target):
+#    array.sort()
+#    lIdx = 0
+#    rIdx = len(array) - 1
 
-   while lIdx < rIdx:
-      current = array[lIdx] + array[rIdx]
-      if current == target:
-         return [array[lIdx], array[rIdx]]
-      elif current < target:
-         lIdx += 1
-      elif current > target:
-         rIdx -= 1
-   return []
+#    while lIdx < rIdx:
+#       current = array[lIdx] + array[rIdx]
+#       if current == target:
+#          return [array[lIdx], array[rIdx]]
+#       elif current < target:
+#          lIdx += 1
+#       elif current > target:
+#          rIdx -= 1
+#    return []
 
-print(twoSum([1,3,5,8,9,2,6,4], 18))
+# print(twoSum([1,3,5,8,9,2,6,4], 18))
+
+# find out if subset array is in the main array
+# in order to be a subset of the arary the elements must appear 
+# in the same order as they do in the main array.
+# create to pointer variables that keep track of the index of each array
+# as we go through the main array we check the current element to teh element of the subset
+# if they match we add 1 to both indexes.  If they do not match we only add 1 to the main arrays index
+# we'll know if the sequence array is a subset of the main array if the length of hte sequence index is == tp the length 
+# of the sequence array
+
+def subSequence(array, sequence):
+   main = 0
+   sub = 0
+
+   while main < len(array) and sub < len(sequence):
+      if array[main] == sequence[sub]:
+         sub += 1
+      main +=1
+   return sub == len(sequence)
+print(subSequence([1,2,5,6,7,8,12,54,63,25],[5,8,25,63]))
