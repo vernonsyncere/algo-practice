6b14ccfb995bfa5851d3dd1917e586ff833532f6
Vernon Lewis <leesyncere@gmail.com>
2020-12-24 10:30:15 -0800

    herre

--- December/Algos.py
+++ December/Algos.py
@@ -275,25 +275,27 @@ import math
 # peakLength = max(peakLength,currentPeak)
 # i = rIdx
 
-def peakLong(array):
-   maxPeak = 0
-   i = 1
-   while i < len(array) -1:
-      isPeak = array[i - 1] < array[i] and array[i] > array[i + 1]
-      if not isPeak:
-         i += 1
-         continue
-      leftIdx = i - 2
-      while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
-         leftIdx -= 1
-      rightIdx = i + 2
-      while rightIdx < len(array) and array[rightIdx] < array[rightIdx - 1]:
-         rightIdx += 1
-      currentPeak = rightIdx - leftIdx - 1
-      maxPeak = max(maxPeak, currentPeak)
-      i = rightIdx
-   return maxPeak
-print(peakLong([1,2,3,3,4,0,10,6,5,-1,-3,2,3]))
+# def peakLong(array):
+#    maxPeak = 0
+#    i = 1
+#    while i < len(array) -1:
+#       isPeak = array[i - 1] < array[i] and array[i] > array[i + 1]
+#       if not isPeak:
+#          i += 1
+#          continue
+#       leftIdx = i - 2
+#       while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
+#          leftIdx -= 1
+#       rightIdx = i + 2
+#       while rightIdx < len(array) and array[rightIdx] < array[rightIdx - 1]:
+#          rightIdx += 1
+#       currentPeak = rightIdx - leftIdx - 1
+#       maxPeak = max(maxPeak, currentPeak)
+#       i = rightIdx
+#    return maxPeak
+# print(peakLong([1,2,3,3,4,0,10,6,5,-1,-3,2,3]))
+
+
    
 
 
